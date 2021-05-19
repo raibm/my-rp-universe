6f1ca80a2d49983af687eb63cdb2c03e5fbaa1c8
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2021-05-18 23:55:12 -0300

    202105182345_ Alterando relacionamento de guilda

---
+++ src/main/java/com/wow/myrpuniverse/entity/pk/PkRelCharacterGuild.java
@@ -0,0 +1,20 @@
+package com.wow.myrpuniverse.entity.pk;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Embeddable
+public class PkRelCharacterEvent implements Serializable {
+
+    private Long idCharacter;
+    private Long idEvent;
+}
